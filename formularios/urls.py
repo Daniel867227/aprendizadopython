1a7d8b763e76d87c3d2d2782e4c7c8bae8976ac2
Daniel <daniel.mdsc91@gmail.com>
2025-04-18 14:16:27 -0300

    continuacao exercicio

--- formularios/urls.py
+++ formularios/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  
+    path('', views.index, name='index'),
     path('usuarios', views.usuarios, name='usuarios'),
-    path('edit_usuarios/<usuarios_id>', views.edit_usuarios, name='edit_usuarios'),
+    path('edit_usuarios/<usuarios_id>/', views.edit_usuarios, name='edit_usuarios'),
+    path('delete_usuarios/<usuarios_id>/', views.delete_usuarios, name='delete_usuarios')
 
 ]
--- formularios/views.py
+++ formularios/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . forms import Usuarios_form
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 from . models import Usuarios
+from django.http import HttpResponseRedirect, HttpResponseNotFound, HttpResponse
+from django.urls import reverse
+
 
 def index(request):
     if request.method != 'POST':
@@ -11,8 +12,10 @@ def index(request):
         form = Usuarios_form(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('index'))
-    
+            frase = 'Usario Cadastrado com sucesso'
+            contexto = {'form':form, 'frase':frase}
+            return render(request, 'formularios/index.html', contexto)
+        
     contexto = {'form': form}
     return render(request, 'formularios/index.html', contexto)
 
@@ -20,22 +23,33 @@ def index(request):
 def usuarios(request):
     usuarios = Usuarios.objects.all()
     contexto = {'usuarios': usuarios}
-    return render(request,'formularios/usuarios.html', contexto)
+    return render(request, 'formularios/usuarios.html', contexto)
 
 
-def edit_usuarios(request, usuarios_id ):
-    usuario = Usuarios.objects.get(id= usuarios_id)
+def edit_usuarios(request, usuarios_id):
+    usuario = Usuarios.objects.get(id=usuarios_id)
+
+    print(usuario)
+    if not usuario:
+        return HttpResponseNotFound(request, 'Não encontrada')
 
     if request.method != 'POST':
         form = Usuarios_form(instance=usuario)
 
     else:
         form = Usuarios_form(instance=usuario, data=request.POST)
-        
+
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('usuarios'))
-    
+
     contexto = {'usuario': usuario, 'form': form}
 
-    return render (request, 'formularios/edit_usuarios.html', contexto)
+    return render(request, 'formularios/edit_usuarios.html', contexto)
+
+def delete_usuarios(request, usuarios_id):
+ 
+    usuario = Usuarios.objects.get(id= usuarios_id)
+    usuario.delete()
+    return HttpResponseRedirect(reverse('usuarios'))
+    
